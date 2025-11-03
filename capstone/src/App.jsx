e3c7077feee5c68a01f9299c4d9fd9f71e75facd
rjablon03 <secrets_crasser.0l@icloud.com>
2025-11-03 12:42:00 -0500

    Attempting to fix signup process

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AuthPage from './pages/AuthPage'
+import SignupPage from './pages/SignupPage'
 import CommonView from './components/CommonView'
 import PrivateRoute from './components/PrivateRoute'
 
@@ -8,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
         <Route
           path="/"
           element={
