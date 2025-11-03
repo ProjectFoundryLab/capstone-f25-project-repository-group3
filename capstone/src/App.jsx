2bbecd1a2579e47f22e495c356eb4b806d9fe7d9
rjablon03 <secrets_crasser.0l@icloud.com>
2025-11-03 13:37:11 -0500

    Fixing bug

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AuthPage from './pages/AuthPage'
-import SignupPage from './pages/SignupPage'
 import CommonView from './components/CommonView'
 import PrivateRoute from './components/PrivateRoute'
 
@@ -9,7 +8,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/sign-up" element={<SignupPage />} />
         <Route
           path="/"
           element={
